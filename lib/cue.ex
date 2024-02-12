5f4e15a07c8a142150d1190289c3ac63c71e6c58
Hugo <hpjm37@gmail.com>
2024-02-12 07:41:09 +0000

    Init commit

---
+++ lib/cue.ex
@@ -0,0 +1,18 @@
+defmodule Cue do
+  @moduledoc """
+  Documentation for `Cue`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cue.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cue_test.exs
@@ -0,0 +1,8 @@
+defmodule CueTest do
+  use ExUnit.Case
+  doctest Cue
+
+  test "greets the world" do
+    assert Cue.hello() == :world
+  end
+end
