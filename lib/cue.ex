3033578fa7cf36e8c55d1536eaf25ea4bb0a7772
Hugo <hpjm37@gmail.com>
2024-02-20 16:50:38 +0800

    Create enqueue!/2 and dequeue/2 functions and dispatch to them from injected module

--- lib/cue.ex
+++ lib/cue.ex
@@ -6,6 +6,42 @@ defmodule Cue do
   @callback handle_job(any()) :: :ok | {:ok, map()} | {:error, any()}
   @callback handle_job_error(any()) :: :ok | {:ok, map()} | {:error, any()}
 
+  def enqueue!(handler, opts) do
+    opts = Keyword.validate!(opts, [:repo, :name, :schedule, :error_handler, run_now: false])
+
+    run_at =
+      if opts[:run_now] do
+        DateTime.utc_now()
+      else
+        Cue.Schemas.Job.next_run_at(opts[:schedule])
+      end
+
+    opts[:repo].insert!(
+      %Cue.Schemas.Job{
+        name: opts[:name],
+        handler: handler,
+        error_handler: opts[:error_handler],
+        run_at: run_at,
+        schedule: opts[:schedule],
+        status: :not_started
+      },
+      on_conflict: :nothing,
+      conflict_target: :name,
+      returning: true
+    )
+  end
+
+  def dequeue(repo, name) do
+    require Ecto.Query
+
+    {count, _returned} =
+      Cue.Schemas.Job
+      |> Ecto.Query.where(name: ^name)
+      |> repo.delete_all()
+
+    count
+  end
+
   defmacro __using__(opts) do
     name = Keyword.get(opts, :name)
     schedule = Keyword.fetch!(opts, :schedule)
@@ -16,7 +52,7 @@ defmodule Cue do
       @repo Application.compile_env!(:cue, :repo)
       @cue_name unquote(name) || String.replace("#{__MODULE__}", ~r/^Elixir\./, "")
 
-      def put_on_queue! do
+      def enqueue! do
         run_at =
           if unquote(run_immediately) do
             DateTime.utc_now()
@@ -24,30 +60,16 @@ defmodule Cue do
             Cue.Schemas.Job.next_run_at(unquote(schedule))
           end
 
-        @repo.insert!(
-          %Cue.Schemas.Job{
-            name: @cue_name,
-            handler: __MODULE__,
-            error_handler: __MODULE__,
-            run_at: run_at,
-            schedule: unquote(schedule),
-            status: :not_started
-          },
-          on_conflict: :nothing,
-          conflict_target: :name,
-          returning: true
+        Cue.enqueue!(__MODULE__,
+          name: @cue_name,
+          error_handler: __MODULE__,
+          schedule: unquote(schedule),
+          repo: @repo
         )
       end
 
-      def delete_from_queue do
-        require Ecto.Query
-
-        {count, _returned} =
-          Cue.Schemas.Job
-          |> Ecto.Query.where(name: @cue_name)
-          |> @repo.delete_all()
-
-        count
+      def dequeue do
+        Cue.dequeue(@repo, @cue_name)
       end
     end
   end
